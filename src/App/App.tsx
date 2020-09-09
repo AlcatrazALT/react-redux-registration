4a725c725810a31d40439f11306449e53aac1ef5
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-09-09 17:03:01 +0700

    feat: Add router to component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { HomePage } from '../HomePage'
+import { LoginPage } from '../LoginPage'
+import { RegisterPage } from '../RegisterPage/RegisterPage'
 
 const App = () => {
-  return <h1>React Template</h1>
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export { App }
